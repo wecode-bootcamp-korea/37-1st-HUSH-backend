c7d74e42fccf9cfa43667b75f87106174e18e873
dyjeong576 <dyjeong576@gmail.com>
2022-09-20 12:26:50 +0900

    dbmate 파일 생성

--- db/migrations/20220919115250_divisions.sql
+++ db/migrations/20220919115250_divisions.sql
@@ -1,8 +1,8 @@
 -- migrate:up
-CREATE TABLE category (
+CREATE TABLE divisions (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
     PRIMARY KEY (id)
 )
 -- migrate:down
-DROP TABLE category;
+DROP TABLE divisions;
