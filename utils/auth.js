29511357cc5281bf4450e50cc589ff701bbd1235
SR-Jung <westeeven0612@gmail.com>
2022-09-22 15:01:48 +0900

    access token 초기셋팅

--- utils/auth.js
+++ utils/auth.js
@@ -1,37 +1,16 @@
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
-const { userService } = require('../services');
+const jwt = require("jsonwebtoken");
+const key = process.env.KEY;
 
-const loginRequired = async (req, res, next) => {
-  // 1) Getting token and check of it's there
-  const accessToken = req.headers.authorization;
 
-  if (!accessToken) {
-    const error = new Error('NEED_ACCESS_TOKEN');
-    error.statusCode = 401;
+const validToken = async(req, res, next) =>{
+    const userToken = req.header("authorization");
+    if( !userToken ) return res.status(400).json({message : "KEY_ERROR" });
+    const decoded = jwt.verify(userToken, key);
+    const {user_id} = decoded;
+    req.body.userId = user_id;
+    return next();
+}
 
-    return res.status(error.statusCode).json({ message: error.message });
-  }
-
-  // 2) Verification token
-  const decoded = await promisify(jwt.verify)(
-    accessToken,
-    process.env.JWT_SECRET
-  );
-
-  // 3) Check if user still exists
-  const user = await userService.getUserById(decoded.id);
-
-  if (!user) {
-    const error = new Error('USER_DOES_NOT_EXIST');
-    error.statusCode = 404;
-
-    return res.status(error.statusCode).json({ message: error.message });
-  }
-
-  // 4) GRANT ACCESS
-  req.user = user;
-  next();
-};
-
-module.exports = { loginRequired };
+module.exports = {
+    validToken
+}
