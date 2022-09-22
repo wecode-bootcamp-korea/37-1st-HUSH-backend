194488c89fc4a21e5ba1ae5b10ca08092a9d2397
SR-Jung <westeeven0612@gmail.com>
2022-09-22 13:46:02 +0900

    주석제거 2차 수정

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,10 +3,8 @@ const { cartService } = require('../services');
 const addCart = async (req, res) => {
   const { productId, quantity, userId } = req.body; //엑세스 토큰 활성화 시 userId 제거
   // const userId = req.user.id;  //엑세스 토큰 활성화 시 주석 제거 (productRouter - loginRequired 활성화 시)
-  // console.log(req.body)    //데이터 확인용
 
   try {
-    // const insertId = await productService.createCart(title, content, userId);
     const addCart = await cartService.addCart(userId, productId, quantity);
 
     res.status(201).json({ addCart });
@@ -15,6 +13,19 @@ const addCart = async (req, res) => {
   }
 };
 
+const listUpCart = async (req, res) => {
+  const { productId, quantity, userId } = req.body;
+
+  try{
+    const listUpCart = await cartService.listUpCart(userId, productId, quantity);
+
+    res.status(201).json({ listUpCart });
+  }catch (error) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
+};
+
 module.exports = {
   addCart,
+  listUpCart,
 };
