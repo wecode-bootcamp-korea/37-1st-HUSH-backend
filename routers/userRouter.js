9d76990e905d99210d22a7a34927474ed1c9fc14
dyjeong576 <dyjeong576@gmail.com>
2022-09-23 15:32:40 +0900

    conflict 해결

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const userController  = require('../controllers/userController');
-<<<<<<< HEAD
 const { validToken } = require('../utils/auth')
-=======
 const { validToken } = require('../utils/auth');
->>>>>>> 0a9da4f925680f30bbdbcd66d59389197a51a014
 
 const router = express.Router();
 
@@ -13,10 +10,7 @@ router.post('/check', userController.checkUser);
 
 router.post('/signin', userController.signIn);
 
-<<<<<<< HEAD
 router.delete('/likes/deletelike', validToken, userController.deleteLike);
-=======
 router.get('/like', validToken, userController.getLikeList);
->>>>>>> 0a9da4f925680f30bbdbcd66d59389197a51a014
 
 module.exports = router;
