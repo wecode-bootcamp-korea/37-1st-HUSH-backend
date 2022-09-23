4cd891f1c35f71eb9558313c85bedb8964e438d3
ninetysixHoon <789ee45@gmail.com>
2022-09-23 13:24:05 +0900

    asterisk 수정

--- models/productsDao.js
+++ models/productsDao.js
@@ -3,7 +3,14 @@ const appDataSource = require('./dataSource')
 const getAllProducts = async () => {
     return await appDataSource.query(`
       SELECT
-          *
+          products.id,
+          products.name,
+          products.price,
+          products.stock,
+          products.thumbnail_image_url,
+          products.updated_at,
+          products.category_id,
+          products.created_at
         FROM products
         `)
   };
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
 const { productsController } = require('../controllers');
-// const { loginRequired } = require('../utils/auth')
+
 const router = express.Router();
 
-// router.get('/showproduct/:productId', productsController.getProduct)
 router.get('/', productsController.getAllProducts)
-// router.post('', loginRequired, postController.createPost)
-// router.patch('/:postId', loginRequired, postController.updatePost)
-// router.delete('/:postId', loginRequired, postController.deletePost)
 
 module.exports = router;
