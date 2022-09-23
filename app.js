2319e4a6e2668f81362f8e40db4f0662ddfb9b59
ninetysixHoon <789ee45@gmail.com>
2022-09-23 15:09:55 +0900

    router 수정

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const express = require('express');
 const cors    = require('cors');
 const app     = express();
 const morgan  = require('morgan');
-const routes = require("./routes");
+const routers = require("./routers");
 const{ globalErrorHandler } = require('./utils/error')
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(routes);
+app.use(routers);
 
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
