c8b6040389bdc664293a14b4eab966eed259ed2d
SeongRyul-Jung <107356126+SeongRyul-Jung@users.noreply.github.com>
2022-09-23 20:17:51 +0900

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,10 +3,10 @@ const { catchAsync }  = require('../utils/error')
 
 const addCart = catchAsync(async (req, res) => {
 
-	const {  product_id, quantity} = req.body
+	const {  productId, quantity} = req.body
 	const userId = req.userId
 
-	const insertId = await cartService.addCart(product_id, quantity, userId)
+	const insertId = await cartService.addCart(productId, quantity, userId)
 
 	res.status(201).json({ insertId })
 })
