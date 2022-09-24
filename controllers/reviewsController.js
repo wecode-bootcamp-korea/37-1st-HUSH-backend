3beb2b538802ef930b2e43397367a6a1b4330670
ninetysixHoon <789ee45@gmail.com>
2022-09-24 16:32:24 +0900

    reviews

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -2,11 +2,9 @@ const { reviewsService } = require('../services')
 const { catchAsync }  = require('../utils/error')
 
 const getreviews = catchAsync(async (req, res) => {
-    // console.log(req.params)
     const {product_id} = req.params; 
-    // console.log(product_id)
     const reviews = await reviewsService.getreviews(product_id);
-    // console.log(reviews);
+
     return res.status(200).json({ data : reviews });
 })
 
