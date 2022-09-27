dbc7079ebf51afacb889e8a0b2993b7065a80ee7
SR-Jung <westeeven0612@gmail.com>
2022-09-27 13:57:33 +0900

    conflict 해결 2차

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,19 +1,6 @@
 const { cartService } = require('../services')
 const { catchAsync }  = require('../utils/error')
 
-<<<<<<< HEAD
-const listDelete = catchAsync(async (req, res) => {
-	const { productId } = req.body
-	const userId = req.userId
-	console.log(userId)		//userId - undefine 해결하면 끝
-	const result = await cartService.listDelete(productId, userId)
-
-	res.status(201).json ({ result })
-})
-
-module.exports = {
-	listDelete,
-=======
 const addCart = catchAsync(async (req, res) => {
 
 	const { productId, quantity } = req.body
@@ -24,7 +11,16 @@ const addCart = catchAsync(async (req, res) => {
 	res.status(201).json({ insertId })
 })
 
+const listDelete = catchAsync(async (req, res) => {
+	const { productId } = req.body
+	const userId = req.userId
+	console.log(userId)		//userId - undefine 해결하면 끝
+	const result = await cartService.listDelete(productId, userId)
+
+	res.status(201).json ({ result })
+})
+
 module.exports = {
 	addCart,
->>>>>>> main
+	listDelete,
 }
