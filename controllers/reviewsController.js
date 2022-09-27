3f82a9785e063b018478d18190c360d358672a1f
ninetysixHoon <789ee45@gmail.com>
2022-09-28 01:12:43 +0900

    before PR

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -5,7 +5,7 @@ const postReviews = catchAsync(async (req, res) => {
     const {product_id} = req.params;
     const content = req.body.content;
     const user_id =  req.userId
-    await reviewsService.postReviews( user_id, product_id, content);    
+   await reviewsService.postReviews( user_id, product_id, content);    
    
     return res.status(200).json({ message: "Review_Success"});
 })
