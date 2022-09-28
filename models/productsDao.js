bc96fbebc4b321d99d009e22e2fc76b6920708c4
ninetysixHoon <789ee45@gmail.com>
2022-09-28 15:06:23 +0900

    수정 완료

--- models/productsDao.js
+++ models/productsDao.js
@@ -3,15 +3,17 @@ const appDataSource = require('./dataSource')
   const getcategoryProducts = async (categoryId) => {
     const result = await appDataSource.query(`
       SELECT
-          id,
-          name,
-          price,
-          stock,
-          category_id,
-          thumbnail_image_url,
-          created_at,
-          updated_at
-        FROM products
+          p.id,
+          p.name,
+          p.price,
+          p.stock,
+          c.name AS category_id,
+          p.thumbnail_image_url,
+          p.created_at,
+          p.updated_at
+        FROM products p
+        join categories c
+        on p.category_id = c.id
         WHERE category_id = ?`, [categoryId]
     )
     
