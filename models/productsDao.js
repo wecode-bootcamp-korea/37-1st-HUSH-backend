36302a89919e4d11aaac80758f2760888c0fdabb
dyjeong576 <dyjeong576@gmail.com>
2022-09-28 13:33:31 +0900

    getproduct 수정

--- models/productsDao.js
+++ models/productsDao.js
@@ -3,26 +3,27 @@ const appDataSource = require('./dataSource')
 const getProduct = async (productId) => {
     const result = await appDataSource.query(`
       SELECT
-          p.id,
-          p.name,
-          p.price,
-          p.stock,
-          c.name as category_name,
-          p.thumbnail_image_url,
-          p.created_at,
-          p.updated_at,
-          i.image_url
-        FROM 
-          products p
-        JOIN 
-          categories c
-        ON  
-          c.id = p.category_id
-        JOIN
-          product_images i
-        ON
-          p.id = i.product_id
-        WHERE p.id = ?`, [productId]
+        p.id,
+        p.name,
+        p.price,
+        p.stock,
+        c.name as category_name,
+        p.thumbnail_image_url,
+        p.created_at,
+        p.updated_at,
+        JSON_ARRAYAGG(i.image_url) AS image_url
+    FROM 
+      products p
+    JOIN 
+      categories c
+    ON  
+      c.id = p.category_id
+    JOIN
+      product_images i
+    ON
+      p.id = i.product_id
+    WHERE 
+      p.id = ?`, [productId]
     )
     
     return result [0]
