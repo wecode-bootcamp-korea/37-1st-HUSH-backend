1a82e81b94d97d19e0d35c4c23d81c6e4ef78dce
ninetysixHoon <789ee45@gmail.com>
2022-09-28 19:31:17 +0900

    ㅇㅇ

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,13 +1,8 @@
 const express = require('express');
-
-const  productsController  = require('../controllers/productsController');
-// const { loginRequired } = require('../utils/auth')
+const { productsController } = require('../controllers');
 const router = express.Router();
 
-//router.get('/showproduct/:productId', productsController.getProduct)
-//router.get('/', productsController.getAllProducts)
-router.get('/showproducts/:categoryId', productsController.getcategoryProducts)
-// router.patch('/:postId', loginRequired, postController.updatePost)
-// router.delete('/:postId', loginRequired, postController.deletePost)
+router.get('/', productsController.getAllProducts)
+router.get('/showproduct/:productId', productsController.getProduct)
 
 module.exports = router;
