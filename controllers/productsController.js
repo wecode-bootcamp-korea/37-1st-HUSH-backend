7ec6b7ce9e5ff5bce258727a6ebed8aa5c5cedfd
ninetysixHoon <789ee45@gmail.com>
2022-09-28 20:12:06 +0900

    asd

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,36 +1,15 @@
 const { productsService } = require('../services')
 const { catchAsync }  = require('../utils/error')
 
-<<<<<<< HEAD
 const getcategoryProducts = catchAsync(async (req, res) => {
-    
+
     const categoryId = req.params.categoryId;
-    
+
     const products = await productsService.getcategoryProducts(categoryId)
-=======
-
-const getAllProducts = catchAsync(async (req, res) => {
-
-	const products = await productsService.getAllProducts();
-
-	return res.status(200).json({ data : products });
-})
-
-const getProduct = catchAsync(async (req, res) => {
-    
-    const productId = req.params.productId;
-    
-    const products = await productsService.getProduct(productId)
->>>>>>> main
 
     res.status(200).json({ products })
 })
 
 module.exports = {
-<<<<<<< HEAD
     getcategoryProducts
-=======
-    getAllProducts,
-    getProduct
->>>>>>> main
 }
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/', productsController.getAllProducts)
 router.get('/showproduct/:productId', productsController.getProduct)
+router.get('/showproducts/:categoryId', productsController.getcategoryProducts)
 
 module.exports = router;
